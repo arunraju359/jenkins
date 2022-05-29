0b835899fb051cb1acdd7ba04d97d4f121f54ba7
arunraju359 <arunraju359@gmail.com>
2022-05-29 18:34:36 +0530

    committing the file for Jenkins

--- seed.groovy
+++ seed.groovy
@@ -1,1 +1,17 @@
 freeStyleJob('MyFirstSeedJob')
+
+
+freeStyleJob('example') {
+    logRotator(-1, 10)
+    jdk('Java 8')
+    scm {
+        github('jenkinsci/job-dsl-plugin', 'master')
+    }
+    triggers {
+        githubPush()
+    }
+    steps {
+        gradle('clean build')
+    }
+    
+}
